c28a79d3e5879b9934b0811209029a2143158b09
Adam Bailey <adamb@trueroll.io>
2025-02-25 18:16:36 -0600

    Revert "clean up"
    This reverts commit 9943d6e1212a974c0a10f3367deb98551f8067c0.

--- index.js
+++ index.js
@@ -223,6 +223,17 @@ function formatTfrTweet(tfr) {
 }
 
 async function updateStoredTfrs(newTfrs, env) {
+	// let existingTfrs = await getStoredTfrs(env);
+
+	// const mergedTfrs = [...existingTfrs];
+	// newTfrs.forEach(newTfr => {
+	// 	if (!mergedTfrs.some(tfr => tfr.notam_id === newTfr.notam_id)) {
+	// 		mergedTfrs.push(newTfr);
+	// 	}
+	// })
+
+	console.log(newTfrs);
+
 	try {
 		await env.TFR_STORAGE.put('tfrs', JSON.stringify(newTfrs));
 		console.log('Stored in KV:', newTfrs.length, 'TFRs');
